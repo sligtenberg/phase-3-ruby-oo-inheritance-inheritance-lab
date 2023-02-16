22afd54d7abd529bb0f25436268fb4272bec563d
stephen ligtenberg <bountyhunter24@gmail.com>
2023-02-15 23:00:51 -0500

    commit tree 9735f9a06433b219ff6b8187a13da9c1c9735584

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative './user'
 
+class Student < User
+
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
+
+    def learn new_knowledge
+        @knowledge << new_knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
